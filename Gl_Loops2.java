ee3b8221d782c4ade9f4b49b2666dc5275b3e6c6
Silky18 <98579356+Silky18@users.noreply.github.com>
2022-01-29 21:54:45 +0530

    Add files via upload

---
+++ Gl_Loops2.java
@@ -0,0 +1,33 @@
+//import java.util.Scanner;
+class Gl_Loops2 {
+public static void main(String[] args) {
+
+
+while(true){
+System.out.println(“ inside while “);
+}
+}
+}
+/*//program ro print 10 to 1
+int i=10;
+while(i>0)
+{
+System.out.println(i);
+i--;
+}
+*/
+//continue n break implimentation
+//int i = 1;
+//infinite for loop
+//for (;; i++) {
+	//continue if no doesn't come in tableof 2
+	//if (i % 2 != 0)continue;
+	//break if no exceeds 20
+	//else 
+		//if (i >40)break;
+	//print loop variable
+//	else
+	//	System.out.println(i);}}}
+	//program to print odd no from 1 to 10S
+//for (int i = 1; i <= 10; i=i+2) {
+//System.out.println(i);}
